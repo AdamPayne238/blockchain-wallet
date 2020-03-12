8d1729bd9fc9e418606c887b41a6100eea810b94
Adam Payne <ajpayne238@gmail.com>
2020-03-12 16:05:18 -0400

    basic structure

---
+++ wallet/src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Balance = () => {
+
+    return(
+        <div>
+            <h1>Balance component</h1>
+        </div>
+    )
+}
+
+export default Balance
---
+++ wallet/src/components/Transactions.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Transactions = () => {
+
+    return(
+        <div>
+            <h1>Transactions component</h1>
+        </div>
+    )
+}
+
+export default Transactions
